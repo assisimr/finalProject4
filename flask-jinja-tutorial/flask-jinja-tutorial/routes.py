bd3f0d030650ea472e8f13da33ae427ee422448c
simran <ska7@njit.edu>
2021-08-02 10:38:33 -0400

    Task- Part2 updated

--- flask-jinja-tutorial/flask-jinja-tutorial/routes.py
+++ flask-jinja-tutorial/flask-jinja-tutorial/routes.py
@@ -19,13 +19,16 @@ def home():
         nav=nav,
         title="Jinja Demo Site",
         description="Smarter page templates with Flask & Jinja."
+                    ""
                     "Layout: The majority of web apps have some sort of structure shared between pages. This can be "
                     "as simple as repeating the same <head> across multiple pages or enforcing a general physical "
                     "structure, such as a standard content + sidebar look. A coherent web app will likely have very "
                     "few unique layouts."
+                    ""
                     "Page: Page templates are the self-explanatory meat and potatoes of templating. While this blog "
                     "has hundreds of posts, each post is merely an instance of a single post template: a uniquely "
                     "assembled section of our site that can be replicated with different data."
+                    ""
                     "Partial: Partials are standalone snippets that can be shared by pages where needed. Think of "
                     "navigation elements, widgets, or anything else designed to be shared across parts of your site.",
         status={'active': True}
